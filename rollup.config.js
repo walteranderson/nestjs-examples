cf9c2b014002a16c512819193ed589aad80f51a0
Walter Anderson <wanderson@remedyurgentcare.com>
2022-06-17 16:25:08 -0500

    adding svelte to nestjs backend

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import svelte from 'rollup-plugin-svelte';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import sveltePreprocess from 'svelte-preprocess';
+
+export default {
+  input: 'src/client/main.ts',
+  output: {
+    file: 'public/bundle/bundle.js',
+  },
+  plugins: [
+    svelte({
+      include: 'src/client/**/*.svelte',
+      preprocess: sveltePreprocess(),
+    }),
+    typescript({ tsconfig: 'src/client/tsconfig.json' }),
+    resolve({ browser: true }),
+  ],
+};
---
+++ src/client/app.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  let name = '';
+
+  onMount(() => {
+    fetch('/api/name')
+      .then(response => response.json())
+      .then(data => name = data.name);
+  });
+
+</script>
+
+{#if name}
+  <p>Hello {name}, from Svelte!</p>
+{:else}
+  <p>Loading</p>
+{/if}
