42a3ddc7a040de64e2e0424f3eb61c4d7b37e42c
Walter Anderson <wanderson@remedyurgentcare.com>
2022-06-21 13:28:14 -0500

    vue boilerplate example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,9 +2,16 @@ import { Get, Controller, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  @Get('/api/name')
+  name() {
+    return {
+      name: 'John Doe',
+    };
+  }
+
   @Get()
-  @Render('index')
+  @Render('*')
   root() {
-    return { message: 'Hello world!' };
+    //
   }
 }
